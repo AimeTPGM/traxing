37c02c7f7c8eda90ee0bf4c6d12cc8a00a8caa20
AimeTPGM <aim.ske09@gmail.com>
2020-10-24 01:50:13 +0700

    Updates

---
+++ precache-manifest.bb6733a5a1df4dd56f87ccc825b072c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3618e7cf155e51d0be45e279b93c4c04",
+    "url": "/traxing/index.html"
+  },
+  {
+    "revision": "f5087de832e0f5032306",
+    "url": "/traxing/static/css/main.5c871564.chunk.css"
+  },
+  {
+    "revision": "cc3c652085d474f224ed",
+    "url": "/traxing/static/js/2.9c656d8d.chunk.js"
+  },
+  {
+    "revision": "ecd8629d666646c91b0a4a937ce8b37d",
+    "url": "/traxing/static/js/2.9c656d8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5087de832e0f5032306",
+    "url": "/traxing/static/js/main.97636290.chunk.js"
+  },
+  {
+    "revision": "77a03efeabd7367e7797",
+    "url": "/traxing/static/js/runtime-main.030be35d.js"
+  },
+  {
+    "revision": "06027ab7e34e2f4e8682f5ec4fc44562",
+    "url": "/traxing/static/media/bangna-new.06027ab7.ttf"
+  }
+]);
